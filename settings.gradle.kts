db1757f2dd8b250d5973aaa9a86e069c830bb5c8
Sam Gammon <sam@elide.dev>
2025-01-06 15:38:07 -0800

    drop! fixup settings fmt
    Signed-off-by: Sam Gammon <sam@elide.dev>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -56,9 +56,7 @@ pluginManagement {
   }
 }
 
-plugins {
-  id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
-}
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.0") }
 
 @Suppress("UnstableApiUsage") dependencyResolutionManagement { repositories { mavenCentral() } }
 
